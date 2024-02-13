6e088dd349a2510d212f3d1d217fd8015ddb2005
Audrey Wang <audrey.rw5683@gmail.com>
2024-02-12 22:12:11 -0800

    Phase 1 added more tests

--- src/test/model/ClinicalTrialTest.java
+++ src/test/model/ClinicalTrialTest.java
@@ -8,25 +8,78 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ClinicalTrialTest {
-    private Patient p1;
-    private Patient p2;
+    private Patient patient1;
+    private Patient patient2;
+    private LocalDate today;
     private ClinicalTrial testTrial;
 
     @BeforeEach
-    void setup () {
-        p1 = new Patient("001", 'F', 28, LocalDate.of(2024, 2, 10));
-        p2 = new Patient("002", 'M', 28, LocalDate.of(2024, 1, 10));
+    void setup() {
+        today = LocalDate.now();
+        patient1 = new Patient("001", 'F', 28, today.minusDays(3));
+        patient2 = new Patient("002", 'M', 65, today.minusDays(500));
         testTrial = new ClinicalTrial("trial1");
     }
 
     @Test
-    void getFollowUpListTest() {
+    void testAddPatient() {
+        assertTrue(testTrial.getPatientList().isEmpty());
+        testTrial.addPatient(patient1);
+        assertEquals(1, testTrial.getPatientNum());
+        assertEquals(patient1, testTrial.getPatientList().get(0));
+        testTrial.addPatient(patient2);
+        assertEquals(2, testTrial.getPatientNum());
+        assertEquals(patient2, testTrial.getPatientList().get(1));
+    }
+
+    @Test
+    void testRemovePatient() {
+        assertTrue(testTrial.getPatientList().isEmpty());
+        testTrial.addPatient(patient1);
+        testTrial.addPatient(patient2);
+        assertEquals(2, testTrial.getPatientNum());
+        testTrial.removePatient(patient1);
+        assertEquals(1, testTrial.getPatientNum());
+        assertEquals(patient2, testTrial.getPatientList().get(0));
+        testTrial.removePatient(patient2);
+        assertEquals(0, testTrial.getPatientNum());
+        assertTrue(testTrial.getPatientList().isEmpty());
+    }
+
+    @Test
+    void testFindatient() {
+        assertNull(testTrial.findPatient("001"));
+        testTrial.addPatient(patient1);
+        testTrial.addPatient(patient2);
+        assertNull(testTrial.findPatient("003"));
+        assertEquals(patient1,testTrial.findPatient("001"));
+    }
+
+    @Test
+    void testGetFollowUpList() {
         assertTrue(testTrial.getFollowUpList().isEmpty());
-        testTrial.addPatient(p1);
-        assertFalse(testTrial.getFollowUpList().isEmpty());
-        assertEquals(p1, testTrial.findPatient("001"));
-        System.out.println(p1.getCurrentPeriod().printPeriod());
+        testTrial.addPatient(patient2);
+        assertTrue(testTrial.getFollowUpList().isEmpty());
+        testTrial.addPatient(patient1);
+        assertEquals(1,testTrial.getFollowUpNum());
+        assertEquals(patient1, testTrial.getFollowUpList().get(0));
+    }
+
+    @Test
+    void testGetCompletedList() {
+        assertTrue(testTrial.getCompletedList().isEmpty());
+        testTrial.addPatient(patient2);
+        testTrial.addPatient(patient1);
+        assertTrue(testTrial.getCompletedList().isEmpty());
+        for (FollowUpPeriod period : patient1.getFollowUpPeriods()) {
+            period.setFollowed();
+        }
+        patient1.checkTrialCompleted();
+        assertFalse(testTrial.getCompletedList().isEmpty());
+        assertEquals(1, testTrial.getCompletedNum());
+        assertEquals(patient1, testTrial.getCompletedList().get(0));
+        patient2.getFollowUpPeriods().get(1).setFollowed();
+        assertEquals(1, testTrial.getCompletedNum());
+        assertEquals(patient1, testTrial.getCompletedList().get(0));
     }
 }
-
-
